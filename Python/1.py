5e24c9663ae75b828b22e3c1c6e5e30a52c50326
Swaroop Acharya <91172033+Swaroop-Acharya@users.noreply.github.com>
2022-09-21 20:28:46 +0530

    init

---
+++ Python/1.py
@@ -0,0 +1,44 @@
+# x=int(10)
+# print(x)
+# x2=float(10)
+# print(x2)
+# x=str(10)
+# print(x)
+
+# Many Values to Multiple Variables
+# x,y=10,29
+# print(x)
+# print(y)
+
+# One Value to Multiple Variables
+# x=y=z="Orange"
+# print(x)
+# print(y)
+# print(z)
+
+# Unpack a Collection
+
+# fruits=["Apple","Banana","Orange"]
+# x,y,z=fruits
+# print(z)
+
+# x="Hello"
+# y="Iam"
+# z="Swaroop"
+# print(x , y , z)
+
+
+# x = "Python       "
+# z = "awesome"
+# print(x +z)
+
+
+
+
+# + -> concatenation
+# , -> outputing multiple Variables
+
+
+# x="Python"
+# y=7
+# print(x,y)
