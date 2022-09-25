f20c62cdd12893079a431a8731a39c442a3e95a7
Swaroop Acharya <91172033+Swaroop-Acharya@users.noreply.github.com>
2022-09-25 12:51:49 +0530

    2

---
+++ C++/2.cpp
@@ -0,0 +1,12 @@
+// Length of the string 
+#include<string>
+#include<iostream>
+using namespace std;
+int main(){
+    // length() and size() both are same
+    string txt ="Hello world";
+    cout<<txt<<endl;
+    cout<<txt.length();
+    cout<<txt.size();
+    return 0;
+}
