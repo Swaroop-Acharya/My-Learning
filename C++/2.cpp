fee8e734e886b42b336c8df51f03ccbf1213d6bc
Swaroop Acharya <91172033+Swaroop-Acharya@users.noreply.github.com>
2022-09-25 13:01:20 +0530

    3

--- C++/2.cpp
+++ C++/2.cpp
@@ -1,3 +1,5 @@
+//String operation
+
 // Length of the string 
 #include<string>
 #include<iostream>
@@ -6,7 +8,18 @@ int main(){
     // length() and size() both are same
     string txt ="Hello world";
     cout<<txt<<endl;
-    cout<<txt.length();
-    cout<<txt.size();
+    cout<<txt.length()<<endl;
+    cout<<txt.size()<<endl;
+
+    // String Access
+    cout<<txt[0]<<endl;
+    for(int i=0;i<txt.length();i++){
+    
+        cout<<txt[i]<<endl;
+    }
+
+    // Change String Characters
+    txt[0]='J';
+    cout<<txt;
     return 0;
 }
