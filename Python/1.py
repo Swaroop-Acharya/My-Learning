384e8d209eeefbf98fd08f06efd78a28a1e9de21
Swaroop Acharya <91172033+Swaroop-Acharya@users.noreply.github.com>
2022-09-25 12:40:26 +0530

    init

--- Python/1.py
+++ Python/1.py
@@ -43,9 +43,64 @@
 # y=7
 # print(x,y)
 
-x="Hello"
- 
-def fuction():
-    print(x+" World")
+# Global Variables
 
-fuction()
+# x="Hello"
+# def fuction():
+#     print(x+" World")
+
+# fuction()
+
+
+
+# def func():
+#     global x 
+#     x="Hi"
+#     print(x , "Swaroop")
+# func()
+
+# print(x)
+
+# Strings are arrays
+# x="Swaroop"
+# print(x[1])
+# for i in range(0,len(x)):
+#     print(x[i])
+
+
+# for x in "Swaroop":
+#     print(x)
+
+
+# Check string
+# x="Hello iam Swaroop"
+# print("o" in x)
+
+# x="Hi iam swaroop"
+# print("f" not in x)
+
+# if "iam" in x:
+#     print("word iam is present")
+
+
+# string methods 
+# x="Swaroop Acharya"
+# y=19
+# x="   SWaro    p   "
+# print(x)
+# print(x.strip())
+
+# print(x.replace("p","f"))
+# print(x.center(90))
+# print(x.casefold())
+# print(x.count("o"))
+
+
+# String Formates
+x="Swaroop Acharya"
+y=19
+w=60
+z="Iam " + x + " {1} "+"and my weight is {0}"
+print(z.format(y,w))
+z="Iam " + x + " {} "+"and my weight is {}"
+print(z.format(y,w))
