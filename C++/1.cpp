2c4a54816cdf338a688b997bc715264da43cc72b
Swaroop Acharya <91172033+Swaroop-Acharya@users.noreply.github.com>
2022-09-25 12:42:24 +0530

    init

---
+++ C++/1.cpp
@@ -0,0 +1,16 @@
+// Binary Operators
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c=0;
+    a=3,b=5;
+    // c=a | b;
+    // c=a & b;
+    // c=a ^ b;
+    // c=~b;
+    // c= a<< 2;
+    // c= a>> 2;
+    cout<<c;
+    return 0;
+}
