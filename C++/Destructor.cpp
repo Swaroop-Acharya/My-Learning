93ad5f7b4ce935ee119e6f9595c53de74d821dde
Swaroop Acharya <91172033+Swaroop-Acharya@users.noreply.github.com>
2022-10-10 21:55:09 +0530

    10-10

--- C++/Destructor.cpp
+++ C++/Destructor.cpp
@@ -1,3 +1,16 @@
 #include<string>
 #include<iostream>
 using namespace std;
+class Sample{
+    public:
+    Sample(){
+        cout<<"Object created"<<endl;
+    }
+    // Destructor
+    ~Sample(){
+        cout<<"Object Destroyed"<<endl;
+    }
+};
+main(){
+    Sample s1,s2,s3;
+}
