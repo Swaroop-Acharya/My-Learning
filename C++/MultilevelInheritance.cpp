d64f96939c384d95b93f7e958216005da93051a9
Swaroop Acharya <91172033+Swaroop-Acharya@users.noreply.github.com>
2022-10-12 11:04:47 +0530

    12-10

--- C++/MultilevelInheritance.cpp
+++ C++/MultilevelInheritance.cpp
@@ -12,11 +12,21 @@ class MyGrandF
 };
 
 class GrandF: public MyGrandF{
-    
-}
+    public:
+    string sirname1="S";
+};
+class MyFather: public GrandF{
+    public:
+    string sirname2="A";
+};
 
 int main()
 {
-
+    MyFather obj;
+    string son="Swaroop";
+    cout<<"My name is "<<son<<endl;
+    cout<<"My Full name will be:"<<obj.sirname2<<" "<<obj.sirname1<<" "<<obj.sirname<<" "<<son<<endl;
+    cout<<"My favourite food is my family food that is"<<endl;
+    obj.familyfood();
     return 0;
 }
