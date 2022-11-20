f142456a2391474c9f893540e37924150f34246b
Swaroop Acharya <91172033+Swaroop-Acharya@users.noreply.github.com>
2022-11-20 11:58:55 +0530

    20-11

---
+++ Development/WEB/ReactJS/Tut1/my-react-app/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Iam Swaroop</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ Development/WEB/ReactJS/Tut1/my-react-app/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+const myFirstElm=<h1>Heloo world Iam Swaroop</h1>
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(myFirstElm);
