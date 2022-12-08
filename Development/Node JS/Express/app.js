80f4f510a2bca733e2fb67aa90b0295aad0676ba
Swaroop Acharya <91172033+Swaroop-Acharya@users.noreply.github.com>
2022-12-08 10:54:41 +0530

    08-12

--- Development/Node JS/Express/app.js
+++ Development/Node JS/Express/app.js
@@ -1,4 +1,5 @@
 const express=require('express');
+const path=require('path');
 const app=express();
 const port=80;
 // Static files are the public files that we want to keep on our server so that anyone can download or use it. 
@@ -6,6 +7,25 @@ const port=80;
 // For serving static fies
 
 app.use("/static",express.static('staticf'));
+
+// Now if we want to serve custom static templates, then we can take the help of template engines.
+//  A template engine (template processor) is a software designed to combine templates with a data model 
+//  to produce result documents. In simple words, if we feed anything to the template engine, it provides 
+//  the custom template output.
+
+//Set template as PUG
+app.set("view engine","pug");
+
+// Set views directory
+app.set("views",path.join(__dirname,"views"));
+
+// create a route to render the demo.pug file.
+app.get("/products",(req,res)=>{
+    res.status(200).render('demo', { title: 'Hey', message: 'Hello there!' })
+});
+
+
+
 
 
 //get request takes two args:- endpoint , callback
@@ -15,12 +35,10 @@ app.get("/",(req,res)=>{
 app.get("/about",(req,res)=>{
     res.send("This is about section of my first app using express");
 });
-app.get("/services",(req,res)=>{
-    res.send("This is service section of my first app using express");
-});
+
 // we can also get the status code using get request
-app.get("/products",(req,res)=>{
-    res.status(200).send("This is about service of my first app using express");
+app.get("/services",(req,res)=>{
+    res.status(200).send("This is service section of my first app using express");
 });
 
 
