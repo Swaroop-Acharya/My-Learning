0d012f2c35c6c90c0daac2dc1050645d12261abb
Swaroop Acharya <91172033+Swaroop-Acharya@users.noreply.github.com>
2023-08-28 20:02:42 +0530

    Done: Revision

--- Development/WEB/30daysOfJS/practise/p3.js
+++ Development/WEB/30daysOfJS/practise/p3.js
@@ -10,3 +10,27 @@ class Person {
   }
 
 const p1 = new Person("Swaroop","Acharya")
+
+
+const countriesAPI = 'https://restcountries.com/v2/all'
+const catsAPI = 'https://api.thecatapi.com/v1/breeds'
+
+
+function largestCountry(data){
+  const country=data.sort((a,b)=>{
+    return b.area-a.area
+  }).filter((ele,i)=>i<10)
+  console.log(country)
+}
+
+async function doWork(){
+  try {
+    
+    const response = await fetch(countriesAPI)
+    const data = await response.json()
+    largestCountry(data)
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+doWork()
